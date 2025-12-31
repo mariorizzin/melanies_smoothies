bc8bd5cfc3568340951d42e36fe109d469f51953
mariorizzin <34253185+mariorizzin@users.noreply.github.com>
2025-12-31 16:55:43 +0100

    Refactor smoothie data fetching and display logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,11 @@ if ingredient_list:
 
    for  fruit_chosen in ingredient_list : 
        ingredients_string += fruit_chosen + ' '
-   #st.write(ingredients_string)
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+       st_df = st.dataframe(data=smoothiefroot_response.json() ,use_container_width = True)
+    
+  #st.write(ingredients_string)
 
-   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-   st_df = st.dataframe(data=smoothiefroot_response.json() ,use_container_width = True)
     
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """'  )"""
