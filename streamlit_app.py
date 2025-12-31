91fe18444b7082cd1f56bd9acef28657b286fb72
mariorizzin <34253185+mariorizzin@users.noreply.github.com>
2025-12-31 15:07:54 +0100

    Refactor Snowflake session connection handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,8 +15,9 @@ name_on_order = st.text_input("Name of Smoothie")
 st.write("The name of the Smoothie will be", name_on_order)
 
 
+cnx = st.connection()
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
